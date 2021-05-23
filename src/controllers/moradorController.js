3f362e1c0c5fbd730b671184109c6c92d7dcd457
Matheus Valbert <matheusvalbert@me.com>
2021-05-23 08:05:58 -0300

    criação rotas modificação dados usuário e otimização no recebimento da imagem

--- src/controllers/moradorController.js
+++ src/controllers/moradorController.js
@@ -5,14 +5,95 @@ const authMiddleware = require('../middlewares/auth');
 const moradorMiddleware = require('../middlewares/morador');
 const multer = require('multer');
 const multerConfig = require('../config/multer');
-const path =  require('path').join(__dirname, '..', '/img');
+const path =  require('path').join(__dirname, '..', '/img/');
+const fs = require('fs');
+
+router.get('/profileImage/:imgName', (req, res) => {
+
+  try {
+    if (fs.existsSync(path + req.params.imgName)) {
+
+      return res.sendFile(path + req.params.imgName);
+    }
+  } catch(err) {
+
+    return res.status(400).send({ error: err });
+  }
+});
 
 router.use(authMiddleware);
 router.use(moradorMiddleware);
 
-router.get('/profileImage/:imgName', (req, res) => {
+router.put('/completeModify', multer(multerConfig).single('file'), (req, res) => {
 
-  return res.sendFile(path + '/' + req.params.imgName);
+  const name = req.body.name;
+  const plate = req.body.plate;
+  const uid = req.body.uid;
+
+  db.query('SELECT * FROM moradores WHERE uid = ?',
+  [uid],
+  (err, result) => {
+    if(err)
+      return res.status.send({ error: 'fail to get moradores' });
+    else {
+      fs.unlink(path + result[0].img_name, (err) => {
+        if (err)
+          return res.status(400).send({ error: 'falha ao apagar usuario' });
+      });
+      db.query('UPDATE moradores SET name = ?, plate = ?, img_name = ? WHERE uid = ?',
+      [name, plate, req.file.filename, uid],
+      (err, result) => {
+        if(err)
+          return res.status.send({ error: 'fail to get moradores' });
+        else {
+          return res.send({ completeModify: true });
+        }
+      });
+    }
+  });
+});
+
+router.patch('/modify', (req, res) => {
+
+  const name = req.body.name;
+  const plate = req.body.plate;
+  const uid = req.body.uid;
+
+  db.query('UPDATE moradores SET name = ?, plate = ? WHERE uid = ?',
+  [name, plate, uid],
+  (err, result) => {
+    if(err)
+      return res.status.send({ error: 'fail to get moradores' });
+    else {
+      return res.send({ modify: true });
+    }
+  });
+});
+
+router.delete('/delete', (req, res) => {
+
+  const uid = req.body.uid;
+
+  db.query('SELECT * FROM moradores WHERE uid = ?',
+  [uid],
+  (err, result) => {
+    if(err)
+      return res.status.send({ error: 'fail to get moradores' });
+    else {
+      fs.unlink(path + result[0].img_name, (err) => {
+        if (err)
+          return res.status(400).send({ error: 'falha ao apagar usuario' });
+      });
+      db.query('DELETE FROM moradores WHERE uid = ?',
+      [uid],
+      (err, result) =>{
+        if(err)
+          return res.status(400).send({ error: 'falha ao apagar usuario' });
+        else
+          return res.send({ moradorDeleted: true });
+      });
+    }
+  });
 });
 
 router.post('/add', multer(multerConfig).single('file'), (req, res) => {
