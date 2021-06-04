ff194e8de26f09b011c994982df37a17f4b254ce
Matheus Valbert <matheusvalbert@me.com>
2021-06-04 19:53:17 -0300

    listasController completo

---
+++ src/controllers/listasController.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../database/mysql');
+const authMiddleware = require('../middlewares/auth');
+const userMiddleware = require('../middlewares/user');
+
+router.use(authMiddleware);
+router.use(userMiddleware);
+
+router.post('/newList', (req, res) => {
+
+  const name = req.body.name;
+  const ids = req.body.ids;
+
+  db.query('INSERT INTO listas (name, ids, number) VALUES (?, ?, ?)',
+  [name, ids, req.number],
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ error: 'insert error' });
+    else
+      return res.send({ inserted: true });
+  });
+});
+
+router.get('/get', (req, res) => {
+  db.query('SELECT * FROM listas WHERE number = ?',
+  [req.number],
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ error: 'fail to get lists' });
+    else
+      return res.send({ result: result });
+  });
+});
+
+router.patch('/modify', (req, res) => {
+
+  const name = req.body.name;
+  const ids = req.body.ids;
+  const uid = req.body.uid;
+
+  db.query('UPDATE listas SET name = ?, ids = ? WHERE uid = ?',
+  [name, ids, uid],
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ error: 'fail to modify list' });
+    else
+      return res.send({ modified: true });
+  });
+});
+
+router.delete('/delete', (req, res) => {
+
+  const uid = req.body.uid;
+
+  db.query('DELETE FROM listas WHERE uid = ?',
+  [uid],
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ error: 'fail to delete' });
+    else
+      return res.send({ deleted: true });
+  });
+});
+
+module.exports = app => app.use('/listas', router);
