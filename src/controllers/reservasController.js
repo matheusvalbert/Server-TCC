f9c000a4057e4fbb520de5c46486515c4c79b1da
Matheus Valbert <matheusvalbert@me.com>
2021-06-07 22:32:58 -0300

    inicio do reservasController, adicionar/remover ambiente

---
+++ src/controllers/reservasController.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../database/mysql');
+const authMiddleware = require('../middlewares/auth');
+const userMiddleware = require('../middlewares/user');
+
+router.use(authMiddleware);
+router.use(userMiddleware);
+
+router.post('/addLocal', (req, res) => {
+
+  if(req.userId !== 1)
+    return res.status(400).send({ insertedAmbiente: 'not authorized' });
+
+  const name = req.body.name;
+
+  db.query('INSERT INTO ambientes (name) VALUES (?)',
+  [name],
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ insertedAmbiente: false });
+    else
+      return res.send({ insertedAmbiente: true });
+  });
+});
+
+router.delete('/deleteLocal', (req, res) => {
+
+  if(req.userId !== 1)
+    return res.status(400).send({ deletedAmbiente: 'not authorized' });
+
+  const name = req.body.name;
+
+  db.query('DELETE FROM ambientes WHERE name = ?',
+  [name],
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ deleteLocal: false });
+    else
+      return res.send({ deleteLocal: true });
+  });
+});
+
+router.get('/getLocal', (req, res) => {
+
+  db.query('SELECT * FROM ambientes',
+  (err, result) => {
+    if(err)
+      return res.status(400).send({ getLocal: false });
+    else {
+
+      let names = [];
+      let id = [];
+
+      for(var i = 0; i < result.length; i++) {
+        names.push(result[i].name);
+        id.push(result[i].uid);
+      }
+
+      const results = { names, id };
+
+      return res.send({ result: results });
+    }
+  });
+});
+
+module.exports = app => app.use('/reservas', router);
